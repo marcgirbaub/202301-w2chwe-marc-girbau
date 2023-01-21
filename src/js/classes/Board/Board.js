9d73e7960a4c3ed3c568986c7847a6e43dddce65
Marc Girbau <marc.girbau94@gmail.com>
2023-01-21 14:32:41 +0100

    Develop getSubArray method

--- src/js/classes/Board/Board.js
+++ src/js/classes/Board/Board.js
@@ -1,10 +1,15 @@
 import Cell from "../Cell/Cell";
 
 class Board {
-  columns = 40;
-  rows = 40;
+  columns;
+  rows;
   board;
   nextGenerationBoard;
+
+  constructor(columns = 40, rows = 40) {
+    this.columns = columns;
+    this.rows = rows;
+  }
 
   randomizeBoard() {
     this.board = new Array(this.columns).fill(
@@ -12,6 +17,17 @@ class Board {
         .fill(null)
         .map(() => new Cell(Math.floor(Math.random() * 2) === 1))
     );
+  }
+
+  getSubArray(row, column) {
+    const newBoard = this.board.slice(row - 1, row + 2);
+
+    newBoard.forEach((array, position) => {
+      newBoard[position] = array.slice(column - 1, column + 2);
+      return newBoard;
+    });
+
+    return newBoard;
   }
 }
 
