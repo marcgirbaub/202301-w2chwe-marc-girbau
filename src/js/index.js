66c91a43eff16d532ef41a88d3c8b1c848291072
Marc Girbau <marc.girbau94@gmail.com>
2023-01-22 10:26:40 +0100

    Fix randomize method to properly create new random Cell instancies

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,24 @@
 import Board from "./classes/Board/Board";
 import boardElement from "./global-variables/global-variables";
 
+const mainContainer = document.getElementsByClassName("main-container")[0];
+
 const startApp = () => {
   const board = new Board();
+
   board.randomizeBoard();
+
   board.transformBoard();
+
   board.updateBoard(boardElement);
+
+  const update = () => {
+    board.buildNextGenerationBoard();
+    board.updateBoard(boardElement);
+  };
+
+  let updateBoard;
+  updateBoard = setInterval(update, 300);
 };
 
 startApp();
