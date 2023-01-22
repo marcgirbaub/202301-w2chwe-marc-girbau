33d437bfee60e561d39c37c1cb50be61eebc16c4
Marc Girbau <marc.girbau94@gmail.com>
2023-01-22 11:44:54 +0100

    Add functionality to interactive buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,52 @@
 import Board from "./classes/Board/Board";
 import boardElement from "./global-variables/global-variables";
 
-const startApp = () => {
-  const board = new Board();
+const playButtonEl = document.getElementsByClassName(
+  "button__interactive--play"
+)[0];
+const stopButtonEl = document.getElementsByClassName(
+  "button__interactive--stop"
+)[0];
+const randomButtonEl = document.getElementsByClassName(
+  "button__interactive--randomize"
+)[0];
 
+const startApp = () => {
   board.randomizeBoard();
 
   board.transformBoard();
 
   board.updateBoard(boardElement);
+};
 
-  const update = () => {
-    board.buildNextGenerationBoard();
-    board.updateBoard(boardElement);
-  };
+let gaming = false;
+let updateBoard;
+const board = new Board();
 
-  setInterval(update, 300);
+const update = () => {
+  board.buildNextGenerationBoard();
+  board.updateBoard(boardElement);
 };
 
 startApp();
+
+playButtonEl.addEventListener("click", () => {
+  if (!gaming) {
+    updateBoard = setInterval(update, 300);
+  }
+
+  gaming = true;
+});
+
+stopButtonEl.addEventListener("click", () => {
+  clearInterval(updateBoard);
+  gaming = false;
+});
+
+randomButtonEl.addEventListener("click", () => {
+  clearInterval(updateBoard);
+  board.randomizeBoard();
+  board.transformBoard();
+  board.updateBoard(boardElement);
+  gaming = false;
+});
